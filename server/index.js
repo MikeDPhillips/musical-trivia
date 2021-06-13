a5bf00e6c60a505f4a5a50b067b4f2234bffca10
Michael Phillips <45827183+MikeDPhillips@users.noreply.github.com>
2021-06-12 23:47:17 -0400

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const express = require("express")
 const PORT = process.env.PORT || 3001;
-
+const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -11,8 +11,11 @@ app.get("/api", (req, res) => {
     res.json({message: "Hello from server!"});
 });
 
+app.get("/test", (req, res) => {
+    res.json({message: "This is just a test"});
+});
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html')));
+    res.sendFile(path.resolve(__dirname, '../client/build/public', 'index.html'))
 });
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
