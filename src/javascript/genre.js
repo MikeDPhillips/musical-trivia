6d3a429f2ea0ec5807e199451d7186decd3d2c1a
Michael Phillips <45827183+MikeDPhillips@users.noreply.github.com>
2021-06-24 00:08:10 -0400

    Rewrote genre page to handle autoplay in safari

--- src/javascript/genre.js
+++ src/javascript/genre.js
@@ -1,4 +1,6 @@
-function sound(surl) 
+let hiphopAudio = document.getElementById('a-rap');
+
+function sound(surl)
 {
 	document.getElementById("myspan").innerHTML=
 	"<embed src=\""+surl+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
@@ -17,3 +19,15 @@ function saveGenre(clicked_id)
 	localStorage.setItem("genre", thegenre);
 
 }
+
+function playSound(clicked_id) {
+	let newId = `audio-${clicked_id}`;
+			let currAudio = document.getElementById(newId);
+	currAudio.play();
+	console.log("you clicked " +newId);
+}
+$('#a-hh').mouseover( function() {
+
+	console.log("Hip hop enetered");
+	hiphopAudio.play();
+});
