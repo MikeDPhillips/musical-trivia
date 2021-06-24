204d4b2d8930dc49d3515b0d6ab2ba0e9c6d9941
Michael Phillips <45827183+MikeDPhillips@users.noreply.github.com>
2021-06-24 19:03:15 -0400

    Screen size adjustment

--- src/javascript/auth_server_snippet.js
+++ src/javascript/auth_server_snippet.js
@@ -7,22 +7,6 @@ const PORT = process.env.PORT || 5000;
 var app = express();
 
 
-// app.get('/credential', function(req, res) {
-
-//     var xhr = new xmlhttprequest();
-//     xhr.open('POST', 'accounts.spotify.com/api/token');
-//     xhr.responseType = 'json';
-//     console.log(xhr);
-//     xhr.setRequestHeader('Authorization', 'Basic efa771d3b7fb42deb7936dbf833141f8:cbeb9c6e4dbf4db0ba252158d1ae27eb');
-//     xhr.onreadystatechange = function() {
-//         console.log(this.readyState);
-//         if (this.readyState == 4) {
-//             var response = this.response;
-//             console.log(this.responseText);
-//         }
-//     };
-//     xhr.send();
-// });
 app.get('/credential', function(req, res) {
     const client_id = 'efa771d3b7fb42deb7936dbf833141f8';
     const client_secret = 'cbeb9c6e4dbf4db0ba252158d1ae27eb';
